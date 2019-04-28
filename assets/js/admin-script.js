844d42c063a758f19404985c0670dd87dde32260
Dmitry <d.i.ma.92@mail.ru>
2019-04-28 13:07:49 +0600

    change settings

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -11,7 +11,7 @@ jQuery(function($) {
                         <option value="agree">Соглашение</option>\
                     </select>\
                 </div>\
-                <div class="span2"><div class="control-label"><label>Обязательно<input type="checkbox" name="require" value="1"></label></div></div>\
+                <div class="span2"><div class="control-label"><label>Обязательно<input type="checkbox" name="required" value="1"></label></div></div>\
                 <div class="span1"><button type="button" class="d-field__remove btn btn-small btn-danger">Удалить</button></div>\
             </div>\
         </div>'),
@@ -31,7 +31,7 @@ jQuery(function($) {
             newField.find('[name=name]').val(elem.name);
             newField.find('[name=label]').val(elem.label);
             newField.find('[name=type]').val(elem.type);
-            newField.find('[name=required]').val(elem.required);
+            newField.find('[name=required]').prop('checked', elem.required);
             fields.append(newField);
             console.log(elem);
         });
@@ -44,13 +44,35 @@ jQuery(function($) {
         e.preventDefault();
         let newField = templateField.clone();
         fields.append(newField);
+        changeSettings();
     });
 
     // Удалить поле
     fields.on('click', '.d-field__remove', function(e) {
         e.preventDefault();
         $(this).closest('.d-field').remove();
+        changeSettings();
     });
+
+    // Обновить поле
+    fields.on('change', function() {
+        changeSettings();
+    });
+
+    // Обновить настройки
+    function changeSettings() {
+        let data = [];
+        fields.find('.d-field').each(function(elem) {
+            let row = {};
+            row.name = $(this).find('[name=name]').val();
+            row.label = $(this).find('[name=label]').val();
+            row.type = $(this).find('[name=type]').val();
+            row.required = $(this).find('[name=required]').is(':checked');
+            data.push(row);
+        });
+        //console.log(JSON.stringify(data));
+        settings.val(JSON.stringify(data));
+    };
 }); 
 /*
 [
