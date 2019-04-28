9dc4a12d9d6f98401bf727b6847702a3c7322cc1
Dmitry <d.i.ma.92@mail.ru>
2019-04-28 12:45:14 +0600

    event add field

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -11,14 +11,17 @@ jQuery(function($) {
                         <option value="agree">Соглашение</option>\
                     </select>\
                 </div>\
-                <div class="span3"><div class="control-label"><label>Обязательно<input type="checkbox" name="require" value="1"></label></div></div>\
+                <div class="span2"><div class="control-label"><label>Обязательно<input type="checkbox" name="require" value="1"></label></div></div>\
+                <div class="span1"><button type="button" class="d-field__remove btn btn-small btn-danger">Удалить</button></div>\
             </div>\
-        </div>')
+        </div>'),
+        fields = $('<div class="d-fields">');
+
+    init();
 
     function init() {
         let value = settings.val(),
-            data = JSON.parse(value),
-            fields = $('<div class="d-fields">');
+            data = JSON.parse(value);
 
         settings.after(fields);
         settings.after('<div class="control-group">');
@@ -27,7 +30,7 @@ jQuery(function($) {
             let newField = templateField.clone();
             newField.find('[name=name]').val(elem.name);
             newField.find('[name=label]').val(elem.label);
-            newField.find('[name=type]').val(elem.type);//.trigger("chosen:updated");
+            newField.find('[name=type]').val(elem.type);
             newField.find('[name=required]').val(elem.required);
             fields.append(newField);
             console.log(elem);
@@ -36,7 +39,13 @@ jQuery(function($) {
         fields.after('<button type="button" class="d-field__add btn btn-small btn-success">Добавить</button>')
     };
 
-    init();
+    // Добавить поля
+    $('.d-field__add').click(function(e) {
+        e.preventDefault();
+        let newField = templateField.clone();
+        fields.append(newField);
+    });
+
 });
 /*
 [
